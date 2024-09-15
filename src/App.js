82cfbd33b9b1414d1966602bd0fc1fee2d10ce60
Drake Owen <drake@reidfarms.com>
2024-09-14 23:58:43 -0500

    Finished homepage... hopefully

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import './App.css';
-import TopBar from './components/TopBar'
-import Nav from './components/Nav';
+import "./App.css";
+import TopBar from "./components/TopBar";
+import Nav from "./components/Nav";
+import Meetings from "./components/Meetings";
 
 function App() {
   return (
     <div className="App">
-     <TopBar />
-     <Nav />
+      <div classname="container">
+        <div className="topbar-component">
+          <TopBar />
+        </div>
+        <div classname="nav-component">
+          <Nav />
+        </div>
+        <div className="meetings-component">
+          <Meetings />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Meetings.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Meetings.css";
+
+const Meetings = () => {
+  return (
+    <div className="meeting-container">
+      <h1>Meetings 2nd and 4th Thursday @ 7pm. Meal @ 6pm.</h1>
+    </div>
+  );
+};
+
+export default Meetings;
