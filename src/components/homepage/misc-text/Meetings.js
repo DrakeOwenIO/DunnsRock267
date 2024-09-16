4e0b3e48d32a9305e9a3aeee509e73ee665988da
Drake Owen <drake@reidfarms.com>
2024-09-15 23:42:52 -0500

    Tested some mobile ideas and fixed text centering when resizing

--- src/components/homepage/misc-text/Meetings.js
+++ src/components/homepage/misc-text/Meetings.js
@@ -7,6 +7,7 @@ const Meetings = () => {
       <div className="meeting-child">
         Meetings 2nd and 4th Thursday @ 7pm. Meal @ 6pm.
       </div>
+      <div className="meeting-child">Practice every Tuesday @ 5:30pm.</div>
     </div>
   );
 };
