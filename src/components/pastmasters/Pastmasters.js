7e4d4c58d70885afb81c7f7427e290630a956b50
Drake Owen <drake@reidfarms.com>
2024-09-26 02:13:41 -0400

    worked on past masters more

--- src/components/pastmasters/Pastmasters.js
+++ src/components/pastmasters/Pastmasters.js
@@ -5,6 +5,25 @@ import "./Pastmasters.css";
 const Pastmasters = () => {
   const svPickens = "/MarkdownData/PMData/SVPickens.md";
   const tlGash = "/MarkdownData/PMData/TLGash.md";
+  const bcLankford = "/MarkdownData/PMData/BCLankford.md";
+
+  const pastMastersArray = [
+    {
+      markdownFile: svPickens,
+      fileName: "S.V Pickens 8/5/1867 - 1868",
+      customNumber: 1,
+    },
+    {
+      markdownFile: tlGash,
+      fileName: "T.L Gash 1869 - June 1873",
+      customNumber: 2,
+    },
+    {
+      markdownFile: bcLankford,
+      fileName: "B. C. Lankford 1873 - June 1875",
+      customNumber: 3,
+    },
+  ];
 
   return (
     <div className="PMContainer">
@@ -12,24 +31,17 @@ const Pastmasters = () => {
       <div className="PMListContainer">
         <ol className="PMList">
           <div className="PMBoxes">
-            <li>
-              <ExpandableBox
-                markdownFile={svPickens}
-                fileName={"S.V Pickens 8/5/1867 - 1868"}
-              />
-            </li>
-            <li>
-              <ExpandableBox
-                markdownFile={tlGash}
-                fileName={"T.L Gash 1869 - June 1878"}
-              />
-            </li>
-            <li>
-              <ExpandableBox
-                markdownFile={tlGash}
-                fileName={"T.L Gash 1869 - June 1878"}
-              />
-            </li>
+            {pastMastersArray.map((master, index) => (
+              <li key={index} className="custom-list-item">
+                <span className="custom-number">
+                  {master.customNumber}. {/* Display custom number */}
+                </span>
+                <ExpandableBox
+                  markdownFile={master.markdownFile}
+                  fileName={master.fileName}
+                />
+              </li>
+            ))}
           </div>
         </ol>
       </div>
