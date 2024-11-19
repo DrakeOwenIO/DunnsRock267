8bcf28a8bf92fc87be2b1e301045b23aac5c125c
Drake Owen <drakeowen@dev-build.local>
2024-11-18 22:56:15 -0500

    added more past masters

---
+++ public/MarkdownData/PMData/Bios/EMAllison.md
@@ -0,0 +1,3 @@
+Worshipful Brother E. M. Allison
+
+E. M. Allison was Dunn’s Rock Lodge’s seventh Master. Worshipful Brother Allison was installed on June 24th 1886 and served until June 24th 1887. Thus far all that is currently known about WB Allison is that after serving as Master he, at some point, moved to Columbia S.C. where he was residing in 1922-23. He passed away on May 11th, 1928. 
--- src/components/pastmasters/Pastmasters.js
+++ src/components/pastmasters/Pastmasters.js
@@ -6,77 +6,107 @@ import AcsSortImg from "../../imgs/acs_sort_icon.png";
 import DesSortImg from "../../imgs/des_sort_icon.png";
 
 
-
 const Pastmasters = () => {
+
+
   const svPickens = "/MarkdownData/PMData/Bios/SVPickens.md";
-  const tlGash = "/MarkdownData/PMData/Bios/TLGash.md";
-  const bcLankford = "/MarkdownData/PMData/Bios/BCLankford.md";
-  const svKing = "/MarkdownData/PMData/Bios/svking.md";
-  const waGnash = "/MarkdownData/PMData/Bios/WAGash.md";
+    const tlGash = "/MarkdownData/PMData/Bios/TLGash.md";
+    const bcLankford = "/MarkdownData/PMData/Bios/BCLankford.md";
+    const svKing = "/MarkdownData/PMData/Bios/svking.md";
+    const waGnash = "/MarkdownData/PMData/Bios/WAGash.md";
+    const tsWood = "/MarkdownData/PMData/Bios/TSWood.md";
+    const emAllison = "/MarkdownData/PMData/Bios/EmAllison.md";
+    const jhLanning = "/MarkdownData/PMData/Bios/JHLanning.md"
+  
+    const pastMastersArray = [
+      {
+        markdownFile: svPickens,
+        fileName: "S.V Pickens 8/5/1867 - 1868",
+        imageSrc: "/MarkdownData/PMData/PMImgs/svpickens.jpg",
+        customNumber: 1,
+      },
+      {
+        markdownFile: tlGash,
+        fileName: "T.L Gash 1869 - June 1873",
+        imageSrc: "/MarkdownData/PMData/PMImgs/tlgash.jpg",
+        customNumber: 2,
+      },
+      {
+        markdownFile: bcLankford,
+        fileName: "B. C. Lankford 1873 - June 1875",
+        imageSrc: "/MarkdownData/PMData/PMImgs/bclankford.jpg",
+        customNumber: 3,
+      },
+      {
+        markdownFile: tlGash,
+        fileName: "T. L. Gash 1875-1876",
+        imageSrc: "/MarkdownData/PMData/PMImgs/tlgash.jpg",
+        customNumber: 2,
+      },
+      {
+        markdownFile: svKing,
+        fileName: "S. V. King 1876-1877",
+        imageSrc: "/MarkdownData/PMData/PMImgs/tlgash.jpg",
+        customNumber: 4,
+      },
+      {
+        markdownFile: waGnash,
+        fileName: "W. A. Gash 1877 - 1879",
+        imageSrc: "/MarkdownData/PMData/PMImgs/WAGash.jpg",
+        customNumber: 5,
+      },
+      {
+        markdownFile: bcLankford,
+        fileName: "B. C. Lankford 1979 - 1880",
+        imageSrc: "/MarkdownData/PMData/PMImgs/bclankford.jpg",
+        customNumber: 3,
+      },
+      {
+        markdownFile: waGnash,
+        fileName: "W. A. Gash 1880 - 1881",
+        imageSrc: "/MarkdownData/PMData/PMImgs/WAGash.jpg",
+        customNumber: 5,
+      },
+      {
+        markdownFile: tsWood,
+        fileName: "T. S. Wood 1881 - 1886",
+        imageSrc: "/MarkdownData/PMData/PMImgs/tswood.jpg",
+        customNumber: 6,
+      },
+      {
+        markdownFile: emAllison,
+        fileName: "E. M. Allison 1886 - 1887",
+        imageSrc: "/MarkdownData/PMData/PMImgs/emallison.jpg",
+        customNumber: 7,
+      },
+      {
+        markdownFile: tsWood,
+        fileName: "T. S. Wood 1887 - 1889",
+        imageSrc: "/MarkdownData/PMData/PMImgs/tswood.jpg",
+        customNumber: 6,
+      },
+      {
+        markdownFile: jhLanning,
+        fileName: "J. H. Lanning 1889 - 1890",
+        customNumber: 8,
+      },
+      {
+        markdownFile: tsWood,
+        fileName: "T. S. Wood 1890 - 1895",
+        imageSrc: "/MarkdownData/PMData/PMImgs/tswood.jpg",
+        customNumber: 6,
+      },
+    ];
 
-  const [sortDescending, setSortDescending] = useState(false);
-
-  const pastMastersArray = [
-    {
-      markdownFile: svPickens,
-      fileName: "S.V Pickens 8/5/1867 - 1868",
-      imageSrc: "/MarkdownData/PMData/PMImgs/svpickens.jpg",
-      customNumber: 1,
-    },
-    {
-      markdownFile: tlGash,
-      fileName: "T.L Gash 1869 - June 1873",
-      imageSrc: "/MarkdownData/PMData/PMImgs/tlgash.jpg",
-      customNumber: 2,
-    },
-    {
-      markdownFile: bcLankford,
-      fileName: "B. C. Lankford 1873 - June 1875",
-      imageSrc: "/MarkdownData/PMData/PMImgs/bclankford.jpg",
-      customNumber: 3,
-    },
-    {
-      markdownFile: tlGash,
-      fileName: "T.L Gash 1875-1876",
-      imageSrc: "/MarkdownData/PMData/PMImgs/tlgash.jpg",
-      customNumber: 2,
-    },
-    {
-      markdownFile: svKing,
-      fileName: "S.V King 1876-1877",
-      imageSrc: "/MarkdownData/PMData/PMImgs/tlgash.jpg",
-      customNumber: 4,
-    },
-    {
-      markdownFile: waGnash,
-      fileName: "W.A Gash 1877 - 1879",
-      imageSrc: "/MarkdownData/PMData/PMImgs/WAGash.jpg",
-      customNumber: 5,
-    },
-    {
-      markdownFile: bcLankford,
-      fileName: "B. C. Lankford 1979 - 1880",
-      imageSrc: "/MarkdownData/PMData/PMImgs/bclankford.jpg",
-      customNumber: 3,
-    },
-    {
-      markdownFile: waGnash,
-      fileName: "W.A Gash 1880 - 1881",
-      imageSrc: "/MarkdownData/PMData/PMImgs/WAGash.jpg",
-      customNumber: 5,
-    },
-  ];
-
-  const sortedMasters = [...pastMastersArray].sort((a, b) => {
-    if (sortDescending) {
-      return b.customNumber - a.customNumber; // Descending order
-    }
-    return a.customNumber - b.customNumber; // Ascending order
-  });
+  const [isReversed, setIsReversed] = useState(false);
 
   const toggleSortOrder = () => {
-    setSortDescending((prev) => !prev);
+    setIsReversed((prev) => !prev);
   };
+
+  const displayOrder = isReversed
+    ? [...pastMastersArray].reverse() // Reverse the original order
+    : pastMastersArray; // Keep the original order
 
   return (
     <div className="PMContainer">
@@ -84,12 +114,12 @@ const Pastmasters = () => {
       <div className="sort-button-container" onClick={toggleSortOrder}>
         <img
           src={
-            sortDescending
+            isReversed
               ? AcsSortImg 
               : DesSortImg
           }
           alt={
-            sortDescending
+            isReversed
               ? "Sort Oldest to Most Current"
               : "Sort Most Current to Oldest"
           }
@@ -100,7 +130,7 @@ const Pastmasters = () => {
       <div className="PMListContainer">
         <ol className="PMList">
           <div className="PMBoxes">
-          {sortedMasters.map((master, index) => (
+          {displayOrder.map((master, index) => (
               <li key={index} className="custom-list-item">
                 <span className="custom-number">
                   {master.customNumber}. {/* Display custom number */}
@@ -117,6 +147,9 @@ const Pastmasters = () => {
       </div>
     </div>
   );
-};
+
+}
+
+  
 
 export default Pastmasters;
