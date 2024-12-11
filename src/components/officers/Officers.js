140683a65496fc6813c33e1e4546a24389a4cf21
Drake Owen <drakeowen@Mac.localdomain>
2024-12-11 18:50:23 -0500

    Saved Work

--- src/components/officers/Officers.js
+++ src/components/officers/Officers.js
@@ -10,22 +10,29 @@ const Officers = () => {
   const officerData = [
     {
       year: "2025",
-      officers: ["Master: Jerry Mann", "Senior Warden: Drake Owen", "Junior Warden: Caleb Abernethy"],
+      officers: [
+        "Master: Jerry Mann", 
+        "Senior Warden: Drake Owen", 
+        "Junior Warden: Caleb Abernethy",
+        "Secretary: Chris Hewes",
+        "Treasurer: Ben Lamm",
+        ""
+    ],
     },
     {
       year: "2024",
       officers: [
         "Master: Christopher Allen Hewes",
-        "Senior Warden: Jerry Dean Mann",
-        "Junior Warden: Drake Kyndall Owen",
-        "Treasurer: Benajah Scott Lamm",
-        "Secretary: William Dana Hawkins",
-        "Chaplain: Wayne Arthur Alley",
-        "Senior Deacon: Caleb Andrew Abernethy",
-        "Junior Deacon: Anthony Wayne Vied",
-        "Senior Steward: Richard Thomas DeKeyser",
-        "Tyler: Timothy Carl Waldrop",
-        "Assistant Secretary: Jerry Dean Mann",
+        "Senior Warden: Jerry Mann",
+        "Junior Warden: Drake Owen",
+        "Treasurer: Ben Lamm",
+        "Secretary: Dana Hawkins",
+        "Chaplain: Wayne Alley",
+        "Senior Deacon: Caleb Abernethy",
+        "Junior Deacon: Anthony Vied",
+        "Senior Steward: Richard DeKeyser",
+        "Tyler: Timothy Waldrop",
+        "Assistant Secretary: Jerry Mann",
       ],
     },
   ];
