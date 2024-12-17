33f39efb8f2ce81b184c8778d463bbca83da4ed3
Drake Owen <drakeowen@dev-build.local>
2024-12-17 00:07:50 -0500

    Structured routes, controllers and models

--- server/routes/pastMastersRoutes.js
+++ server/routes/pastMastersRoutes.js
@@ -1,10 +1,19 @@
 // server/routes/pastMastersRoutes.js
-const express = require("express");
-const pastMastersController = require("../controllers/pastMastersController");
-
+const express = require('express');
+const db = require('../config/db');
 const router = express.Router();
 
-// Route to get past masters data
-router.get("/api/pastmasters", pastMastersController.getPastMastersData);
+// Endpoint to fetch past masters
+router.get('/api/pastmasters', (req, res) => {
+  const query = 'SELECT MasterNumber, Name, StartYear, EndYear, Bio, ImageURL FROM PastMastersList';
+  
+  db.query(query, (err, results) => {
+    if (err) {
+      console.error('Error fetching past masters:', err);
+      return res.status(500).json({ message: 'Error fetching past masters' });
+    }
+    res.status(200).json(results);
+  });
+});
 
 module.exports = router;
